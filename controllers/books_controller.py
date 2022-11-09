7cd335f8e0241c728fb974443f8118aadc7cdc2a
Alistair <alistair13_cooke@hotmail.com>
2022-11-09 20:56:03 +0000

    added html, homepage and books page listing author and books

--- controllers/books_controller.py
+++ controllers/books_controller.py
@@ -1,2 +1,11 @@
 from flask import Flask, render_template
 from repositories import author_repository
+from repositories import book_repository
+from flask import Blueprint
+
+books_blueprint = Blueprint("books", __name__)
+
+@books_blueprint.route("/books")
+def books():
+    books = book_repository.select_all()
+    return render_template("books/index.html", all_books = books)
